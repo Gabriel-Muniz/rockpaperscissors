335ba976e50048906318280f51cdf7e990f7da3a
Gabriel-Muniz <56240854+Gabriel-Muniz@users.noreply.github.com>
2024-08-13 14:30:20 -0300

    Add: logic to get human choice

--- script.js
+++ script.js
@@ -1,9 +1,29 @@
 // Get a random number from 0 to 3;
-let getRandomNumber = () => Math.floor((Math.random() * 3) + 1)
+let getRandomNumber = () => Math.floor(Math.random() * 3 + 1);
 // Get a choice between rock, paper and scissor using the random number
-let pieces = ['rock', 'paper', 'scissors'];
+let pieces = ["rock", "paper", "scissors"];
 
 function getComputerChoice() {
   let computerChoice = getRandomNumber();
   return computerChoice;
+}
+
+function getHumanChoice() {
+  let humanChoice;
+  do {
+    humanChoice = prompt("Rock, paper or scissors?").toLowerCase();
+  } while (checkHumanChoice(humanChoice));
+
+  return humanChoice;
+}
+
+function checkHumanChoice(humanChoice) {
+  if (
+    humanChoice != "rock" &&
+    humanChoice != "paper" &&
+    humanChoice != "scissors"
+  ) {
+    return true;
+  }
+  return false;
 }
