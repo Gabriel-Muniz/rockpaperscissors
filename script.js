bc4b1733bba6d9b84fac1c815e135c7ddd810c31
Gabriel-Muniz <56240854+Gabriel-Muniz@users.noreply.github.com>
2024-08-13 14:09:29 -0300

    Add: logic to get computer choice

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-console.log("Hello World!");
+// Get a random number from 0 to 3;
+let getRandomNumber = () => Math.floor((Math.random() * 3) + 1)
+// Get a choice between rock, paper and scissor using the random number
+let pieces = ['rock', 'paper', 'scissors'];
+
+function getComputerChoice() {
+  let computerChoice = getRandomNumber();
+  return computerChoice;
+}
