48f685686f2ab7d609fb11bba9bb1bbc6a938f36
Gabriel-Muniz <56240854+Gabriel-Muniz@users.noreply.github.com>
2026-08-04 13:27:32 -0300

    Refac: change replay button class name
    uses the right convention for buttons class name, btn instead of button

--- script.js
+++ script.js
@@ -110,7 +110,7 @@ const winnerHandler = () => {
     const replayContainer = document.createElement('div');
     replayContainer.classList.add('replay-container')
     const replayBtn = document.createElement('button');
-    replayBtn.classList.add('replay-button')
+    replayBtn.classList.add('replay-btn')
 
     if (humanScore > computerScore) {
         winnerContainer.textContent = `Congrats you won!`
